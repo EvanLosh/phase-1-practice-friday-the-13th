e9273ce27b1ac3f20fb45a79d6ef8f77f633e293
Evan Losh <evand.losh@gmail.com>
2023-11-07 14:37:34 -0500

    renderMovie function and fetch data

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// Challenge 1
+// For each movie returned from http://localhost:3000/movies create an image and add it to the movie-list nav element.
+
+function renderMovie(x) {
+    // get the parent element for the movie list
+    const moviesNav = document.getElementById("movie-list")
+    console.log(moviesNav)
+    // Create an unordered list for movie images
+    const moviesList = document.createElement("ul")
+    // append a ul to the nav element
+    moviesNav.append(moviesList)
+    // create a list item for each image. Each list item contains one image.
+    const listItem = document.createElement("li") // for now, we will do just a single image
+    // append the list item with an image tag
+    moviesList.append(listItem)
+    // add an image tag
+    const image = document.createElement("img")
+    image.src = x[0].image
+    listItem.append(image)
+    // add a source link to the image tag
+}
+
+
+fetch("http://localhost:3000/movies")
+    .then(res => res.json())
+    .then((res) => {
+        renderMovie(res)
+    })
+
+// function getLink(x) {
+//     return fetch("http://localhost:3000/movies")
+//         .then(res => res.json())
+//         .then((res) => { res[x].image })
+//     // .then(res => console.log(res[0].image))
+// }
+
+// console.log(imageLink)
